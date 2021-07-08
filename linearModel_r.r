0e18752116b3c9cecbbdaceda63c338d91db8354
Kayla Young <young.3193@osu.edu>
2021-07-08 16:22:47 +0000

    First Commit

--- linearModel_r.r
+++ linearModel_r.r
@@ -2,9 +2,11 @@ library(ggplot2)
 
 data = read.csv("regrex1.csv")
 
+png("scatter.png")
 scatter = ggplot(data, aes(x = x, y = y)) +
             geom_point()
 scatter
+dev.off()
 
 png("linreg.png") #open device called png
 linreg = ggplot(data, aes(x = x, y = y)) +
