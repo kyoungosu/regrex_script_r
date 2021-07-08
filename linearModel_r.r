ff74d540ae34492d76884da987abde9fcc4b71b0
Kayla Young <young.3193@osu.edu>
2021-07-08 18:44:25 +0000

    First Commit

--- linearModel_r.r
+++ linearModel_r.r
@@ -1,10 +1,12 @@
 library(ggplot2)
 
-args <- commandArgs(trailingOnly = TRUE)
+#args <- commandArgs(trailingOnly = TRUE)
 
-read.csv(args[1])
+#read.csv(args[1])
 
-regrex1 = read.csv(args[1])
+#regrex1 = read.csv(args[1])
+
+regrex1 = read.csv("regrex1.csv")
 
 png("r_orig.png")
 r_orig = ggplot(regrex1, aes(x = x, y = y)) +
@@ -16,5 +18,5 @@ png("r_lm.png") #open device called png
 r_lm = ggplot(regrex1, aes(x = x, y = y)) +
             geom_point() +
             geom_smooth(method = "lm", se = FALSE)
-r_orig
+r_lm
 dev.off()
